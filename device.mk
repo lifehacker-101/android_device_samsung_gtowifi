b0e8c9f2a07e01591d284f5948e973829e565184
Han Sol Jin <hansol@hansol.ca>
2024-07-20 21:14:47 -0700

    gtowifi: update to T295XXS6CWG6
    gtoxx-user 11 RP1A.200720.012 T295XXS6CWG6 release-keys
    
    Vendor SPL: 2023-05-01
    
    Change-Id: I09d56d9caf3f0b121d5b9a0a62ffcfa85b8db5ae

--- device.mk
+++ device.mk
@@ -242,8 +242,7 @@ PRODUCT_PACKAGES += \
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
-    $(LOCAL_PATH)/configs/public.libraries.txt.backup:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt.backup
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # QTI
 PRODUCT_COPY_FILES += \
